94d95771350d06a0bd9176980c44ac2e1e1e5e7f
Arturik <artur.vascuk@gmail.com>
2026-05-12 17:52:03 +0300

    pokyciai

--- vector.cpp
+++ vector.cpp
@@ -1,7 +1,86 @@
-#include <library.h>
+#include <algorithm>
+#include <chrono>
+#include <cctype>
+#include <cstddef>
+#include <deque>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <random>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
 
 template <typename T>
 class MyVector{
-    T* elem[];
-    
+    private:
+        size_t size;
+        size_t capacity;
+        T* data;
+        void resize(){ //padidina capacity vektoriui
+            capacity = capacity * 2;
+            T * newData = new T[capacity];
+            for(size_t i=0; i<size; i++){
+                newData[i] = data[i];
+            }
+            delete[] data;
+            data = newData;
+        }
+    public:
+        MyVector(): //vektoriaus konstruktorius
+        size(0),
+        capacity(1),
+        data(new T[capacity]){}
+
+        void push_back(T val){ //Iraso nauja kintamaji i vektorio funkcija
+            if (size == capacity) resize();
+            data[size] = val;
+            size++;
+        }
+
+        void pop_back(){ //istrina nari vektoriaus gale
+            if(size > 0) size--;
+        }
+
+        bool empty(){ //patikrina ar vektorius tuscias
+            return size == 0;
+        }
+
+        size_t get_size(){ //grazina vektoriaus dydi
+            return size;
+        }
+
+        void clear(){ //isvalo vektoriu
+            size = 0;
+            capacity = 0;
+        }
+
+        T & operator[](size_t index){ //grazina vektoriaus reiksme duotam indexe
+            return data[index];
+        }
+
+        ~MyVector(){ //destruktorius
+            delete[] data;
+            size = 0;
+            capacity = 0;
+        }
 };
+
+int main(){
+    MyVector<int> vec;
+    for(int i=0; i<5; i++){
+        vec.push_back(i);
+    }
+    for(int i=4; i>=0; i--){
+        std::cout << vec[i] << '\n';
+    }
+    vec.clear();
+    std::cout << vec.empty();
+}
